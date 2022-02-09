3f650f93be99cb89c82f426c12cfb8624e064f05
Boody <cfbuddy124@gmail.com>
2022-02-10 00:23:44 +0200

    add app title

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, IconButton } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import { useThemeSwitcher, themeTypes } from "../context/themeSwitcher";
 
@@ -7,13 +7,17 @@ const NavBar = () => {
 
   return (
     <AppBar>
-      <Toolbar sx={{ display: "flex", justifyContent: "right" }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "center" }}>
+        <Typography variant="h6" component="label">
+          Typing Speed Test
+        </Typography>
         <IconButton
           onClick={() =>
             theme === themeTypes.dark
               ? setTheme(themeTypes.light)
               : setTheme(themeTypes.dark)
           }
+          sx={{ position: "absolute", top: "9px", right: "8px" }}
         >
           <Brightness4Icon />
         </IconButton>
