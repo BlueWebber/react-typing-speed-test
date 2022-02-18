a22fa5effb06c3a629df8aa7f48b9122ca751647
Boody <cfbuddy124@gmail.com>
2022-02-19 00:21:25 +0200

    separate main from typingTest

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,89 +1,20 @@
 import React from "react";
-import {
-  Paper,
-  Box,
-  Tooltip,
-  Typography,
-  IconButton,
-  Container,
-} from "@mui/material";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import SpeedData from "./speedData";
-import CircleTimer from "./circleTimer";
-import TextInput from "./textInput";
+import { Paper, Container } from "@mui/material";
 import Settings from "./settings";
-import { getTime } from "../helpers";
+import TypingTest from "./typingTest";
 
 const Main = () => {
-  const [isPlaying, setIsPlaying] = React.useState(false);
-  const [statistics, setStatistics] = React.useState({
-    wpm: 0,
-    cpm: 0,
-    accuracy: 0,
-  });
   const [settingsOpen, setSettingsOpen] = React.useState(false);
-
-  const duration = React.useMemo(getTime, []);
-
-  React.useEffect(() => {
-    return () => {
-      setIsPlaying(false);
-    };
-  }, []);
 
   return (
     <Container
       sx={{
-        marginTop: "20vh",
+        marginTop: settingsOpen ? "10vh" : "20vh",
       }}
     >
       <Paper sx={{ display: "flex", flexDirection: "column", borderRadius: 2 }}>
         {!settingsOpen ? (
-          <>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                mt: 1,
-                mb: 2,
-                position: "relative",
-              }}
-            >
-              <Typography variant="h5" component="h1">
-                Test your typing speed
-              </Typography>
-              <Tooltip title="Configure">
-                <IconButton
-                  sx={{
-                    position: "absolute",
-                    top: 0,
-                    right: 4,
-                  }}
-                  onClick={() => setSettingsOpen(true)}
-                >
-                  <SettingsOutlinedIcon />
-                </IconButton>
-              </Tooltip>
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-around",
-                alignItems: "center",
-              }}
-            >
-              <CircleTimer duration={duration} isPlaying={isPlaying} />
-              <SpeedData title="words/min">{statistics.wpm}</SpeedData>
-              <SpeedData title="chars/min">{statistics.cpm}</SpeedData>
-              <SpeedData title="accuracy %">{statistics.accuracy}</SpeedData>
-            </Box>
-            <TextInput
-              isPlaying={isPlaying}
-              setIsPlaying={setIsPlaying}
-              setStatistics={setStatistics}
-              duration={duration}
-            />
-          </>
+          <TypingTest setSettingsOpen={setSettingsOpen} />
         ) : (
           <Settings setSettingsOpen={setSettingsOpen} />
         )}
